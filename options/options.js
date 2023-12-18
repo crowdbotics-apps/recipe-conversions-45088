41ab93528acf28110fdd337643fefc3379562d02
Crowdbotics <team@crowdbotics.com>
2023-12-18 22:49:17 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "recipe-conversions-45088",
+  url: "https://recipe-conversions-45088.botics.co",
+  api: "https://recipe-conversions-45088.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
